faae8cfbc13b26ba621e4f8f3274c48645c14677
nirmala <nirmala@surf.net.np>
2018-12-29 16:49:47 +0100

    setup user input layout in js and css file

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="containerWrapper">
         <h1>hello</h1>
+        <div className="searchArea">
+          <input type="text" />
+          <button id="search">Search</button>
+        </div>
       </div>
     );
   }
